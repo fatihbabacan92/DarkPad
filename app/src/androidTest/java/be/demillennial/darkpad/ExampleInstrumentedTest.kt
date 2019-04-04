f46a07224f80619f0445ee4735b6faaf44055362
Fatih <fatihb@live.be>
2019-04-04 19:14:09 +0200

    Updated Copyright

--- app/src/androidTest/java/be/demillennial/darkpad/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/be/demillennial/darkpad/ExampleInstrumentedTest.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Fatih Babacan 2019.
+ * DarkPad is created by Fatih Babacan and released under the GPL3 license.
+ * Please refer to the GPL3 license for additional information.
+ */
+
 package be.demillennial.darkpad
 
 import android.support.test.InstrumentationRegistry
