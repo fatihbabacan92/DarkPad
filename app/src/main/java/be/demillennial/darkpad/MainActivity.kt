d9b863f709007f72114a80bd229c1d535471c8ef
Fatih <fatihb@live.be>
2020-02-11 15:08:59 +0100

    Fixed MainActivity

--- app/src/main/java/be/demillennial/darkpad/MainActivity.kt
+++ app/src/main/java/be/demillennial/darkpad/MainActivity.kt
@@ -13,9 +13,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
 import be.demillennial.darkpad.Data.NotesDb
 import be.demillennial.darkpad.Notes.Note
 import be.demillennial.darkpad.Notes.NoteAdapter
+import be.demillennial.darkpad.Notes.NotesFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_notes.*
@@ -23,42 +27,42 @@ import kotlinx.android.synthetic.main.item_list_note.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var noteAdapter: RecyclerView.Adapter<NoteAdapter.ViewHolder>? = null
+    private var content: FrameLayout? = null
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_note -> {
+                //TODO: Change Titles?
+                //message.setText(R.string.title_alarm)
+                val fragment = NotesFragment.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_settings -> {
+                //message.setText(R.string.title_settings)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        layoutManager = LinearLayoutManager(this)
-        recycler_view.layoutManager = layoutManager
+        content = findViewById(R.id.content)
+        val navigation: BottomNavigationView = findViewById(R.id.navigation)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-        val notes = NotesDb(this).getAll()
-
-        noteAdapter = NoteAdapter(notes, this)
-        recycler_view.adapter = noteAdapter
-
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        val fragment = NotesFragment.Companion.newInstance()
+        addFragment(fragment)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
+            .replace(R.id.content, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(fragment.javaClass.simpleName)
+            .commit()
     }
 }
