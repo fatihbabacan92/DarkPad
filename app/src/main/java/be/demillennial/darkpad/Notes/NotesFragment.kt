82f890a1adafa0b0b5cc2f7ab0b0333606547b16
Fatih <fatihb@live.be>
2020-02-11 13:11:28 +0100

    Created fragment for notes

--- app/src/main/java/be/demillennial/darkpad/Notes/NotesFragment.kt
+++ app/src/main/java/be/demillennial/darkpad/Notes/NotesFragment.kt
@@ -6,8 +6,49 @@
 
 package be.demillennial.darkpad.Notes
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import be.demillennial.darkpad.Data.NotesDb
+import be.demillennial.darkpad.R
+import kotlinx.android.synthetic.main.fragment_notes.*
 
 class NotesFragment : Fragment() {
 
+    private lateinit var noteAdapter: NoteAdapter
+    private lateinit var  notesDb: NotesDb
+
+    companion object {
+        fun newInstance(): NotesFragment {
+            var notesFragment = NotesFragment()
+            var args = Bundle()
+            notesFragment.arguments = args
+            return notesFragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView = inflater!!.inflate(R.layout.fragment_notes, container, false)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val context: Context? = super.getContext()
+        val noteContext: Context = activity!!.applicationContext!!
+        notesDb = NotesDb(noteContext)
+        noteAdapter = NoteAdapter(notesDb.getAll(), context)
+        notes_list.adapter = noteAdapter
+    }
 }
